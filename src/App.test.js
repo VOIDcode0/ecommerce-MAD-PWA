3821b4cf9996e7fbb68e11fff08a026ea196725a
VOIDcode0 <shivamsarang@acpce.ac.in>
2026-03-18 23:08:43 +0530

    Delete src directory

--- src/App.test.js
+++
@@ -1,7 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders e-commerce store heading', () => {
-  render(<App />);
-  expect(screen.getByText(/e-commerce store/i)).toBeInTheDocument();
-});
